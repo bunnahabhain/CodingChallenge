a4435b26b98f50eb196ec41774ff35dc13d48c2d
David Lewis <david@davidsfolly.com>
2023-01-21 19:44:45 -0500

    Initial Commit

---
+++ CodingChallenge/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CodingChallenge
+//
+//  Created by David Lewis on 2023-01-21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
